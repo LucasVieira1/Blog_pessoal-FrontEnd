e18db5c3fdeade3bb7a6bda21308dd363bdf02ca
LucasVieira1 <vieiraalucas1@gmail.com>
2022-03-22 13:20:43 -0300

    indroducao ao menu e rodape

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+
+import Navbar from './components/estaticos/navbar/Navbar'
+import Footer from './components/estaticos/footer/Footer';
 import Home from "./paginas/home/Home"
 import './App.css';
 
 function App() {
   return (
-  <Home />
-    );
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </>
+  );
 }
 
 export default App;
