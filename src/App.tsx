69e548908524002459a171587f16f740a3e76d41
LucasVieira1 <vieiraalucas1@gmail.com>
2022-03-24 14:38:19 -0300

    implementando login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Navbar from './components/estaticos/navbar/Navbar'
+import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import Home from "./paginas/home/Home"
+import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
 import './App.css';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
-    </>
+    <Router>
+      <Navbar />
+
+      <Switch>
+        <div style={{ minHeight: "100vh" }}>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </div>
+      </Switch>
+
+      <Footer />
+    </Router>
   );
 }
 
