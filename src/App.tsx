116c5050d4eab5710ca0c1ffaf7ff920ce7de559
LucasVieira1 <vieiraalucas1@gmail.com>
2022-03-31 15:05:47 -0300

    iniciando CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
 import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
   return (
@@ -35,6 +39,26 @@ function App() {
           <Route path="/posts">
             <ListaPostagem />
           </Route>
+
+          <Route exact path='/formularioPostagem'>
+            <CadastroPost />
+          </Route>
+          <Route exact path='/formularioPostagem/:id'>
+            <CadastroPost />
+          </Route>
+          <Route exact path='/formularioTema'>
+            <CadastroTema />
+          </Route>
+          <Route exact path='/formularioTema/:id'>
+            <CadastroTema />
+          </Route>
+          <Route path='/deletarPostagem/:id'>
+            <DeletarPostagem />
+          </Route>
+          <Route path='/deletarTema/:id'>
+            <DeletarTema />
+          </Route>
+          
         </div>
       </Switch>
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Grid, Typography, Box, Button } from '@material-ui/core';
 import TabPostagem from "../../components/postagens/tabpostagem/TabPostagem";
 
 import "./Home.css";
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
+import { useHistory } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 
 function Home() {
+
+    let history = useHistory();
+    const [token, setToken] = useLocalStorage('token');
+    
+    useEffect(() => {
+      if (token == "") {
+          alert("Você precisa estar logado")
+          history.push("/login")
+  
+      }
+  }, [token])
+  
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className="caixa">
+              <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="titulo">Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo">expresse aqui os seus pensamentos e opiniões!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
+                            <ModalPostagem />
                         </Box>
-                        <Button variant="outlined" className="botton">Ver Postagens</Button>
+                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
-                <Grid xs={12} className="postagem">
-                    <TabPostagem/>
+                <Grid xs={12} className='postagens'>
+                    <TabPostagem />
                 </Grid>
-            </Grid>        </>
+            </Grid>     
+              </>
 
     );
 }
